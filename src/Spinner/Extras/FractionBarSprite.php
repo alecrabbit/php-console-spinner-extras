9e0ac70503b8525e7a9ceb0f777c6e3b0054bbc1
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-08 16:04:51 +0200

    ~wp

---
+++ src/Spinner/Extras/FractionBarSprite.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Spinner\Extras;
+
+use AlecRabbit\Spinner\Extras\A\AFractionBarSprite;
+
+final readonly class FractionBarSprite extends AFractionBarSprite
+{
+
+}
---
+++ tests/Unit/Spinner/Core/Procedure/A/Override/FractionBarSprite.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Tests\Spinner\Unit\Spinner\Core\Procedure\A\Override;
+
+use AlecRabbit\Spinner\Extras\A\AFractionBarSprite;
+
+final readonly class FractionBarSprite extends AFractionBarSprite
+{
+
+}
