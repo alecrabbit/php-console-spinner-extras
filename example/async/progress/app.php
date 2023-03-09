63065631d9caef7d72fc4d402956fb4433841d08
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-09 09:49:54 +0200

    ~wp

--- example/async/progress/app.php
+++ example/async/progress/app.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use AlecRabbit\Spinner\Core\Interval;
-use AlecRabbit\Spinner\Core\Loop\ReactLoopProbe;
 use AlecRabbit\Spinner\Extras\FractionValue;
 use AlecRabbit\Spinner\Factory;
 use AlecRabbit\Spinner\Factory\DefaultsFactory;
-use AlecRabbit\Spinner\Core\Loop\RevoltLoopProbe;
 
 require_once __DIR__ . '/../bootstrap.async.php';
 
