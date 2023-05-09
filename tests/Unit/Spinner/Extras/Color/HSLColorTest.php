5834acf4f52eb60e88702a33d8e70d47989a1967
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-05-09 15:13:54 +0300

    ~

--- tests/Unit/Spinner/Extras/Color/HSLColorTest.php
+++ tests/Unit/Spinner/Extras/Color/HSLColorTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 
-namespace AlecRabbit\Tests\Unit\Spinner\Contract\Color;
+namespace AlecRabbit\Tests\Unit\Spinner\Extras\Color;
 
 use AlecRabbit\Spinner\Exception\InvalidArgumentException;
 use AlecRabbit\Spinner\Extras\Color\HSLColor;
