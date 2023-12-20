65d1cfa1de428b3fff278c7baba06759ee7d20a2
Oleksii <oleksii.druzhchenko@gmail.com>
2023-12-20 17:24:01 +0200

    ~

--- src/Spinner/Extras/Frame/StylingFrame.php
+++ src/Spinner/Extras/Frame/StylingFrame.php
@@ -4,25 +4,30 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Extras\Frame;
 
-use AlecRabbit\Spinner\Core\A\AFrame;
+use AlecRabbit\Spinner\Exception\LogicException;
 use AlecRabbit\Spinner\Extras\Contract\IStylingFrame;
 use AlecRabbit\Spinner\Extras\Contract\Style\IStyle;
 
-final readonly class StylingFrame extends AFrame implements IStylingFrame
+final readonly class StylingFrame implements IStylingFrame
 {
     public function __construct(
         private IStyle $style,
-        ?string $sequence = null,
-        ?int $width = null,
+
     ) {
-        parent::__construct(
-            sequence: $sequence ?? '%s',
-            width: $width ?? 0,
-        );
     }
 
     public function getStyle(): IStyle
     {
         return $this->style;
+    }
+
+    public function getSequence(): string
+    {
+        throw new LogicException('Should not be called.');
+    }
+
+    public function getWidth(): int
+    {
+        throw new LogicException('Should not be called.');
     }
 }
